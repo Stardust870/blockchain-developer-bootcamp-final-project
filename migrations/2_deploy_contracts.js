4fafca72cfa735733c529c301f2c45209350f4fb
Stardust870 <88218246+Stardust870@users.noreply.github.com>
2022-01-13 20:46:25 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,3 @@
-// const Rentals = artifacts.require("./Rentals.sol");
-
-// module.exports = function (deployer) {
-//   deployer.deploy(Rentals);
-// };
-
 const Tutors = artifacts.require("./Tutors.sol");
 
 module.exports = function (deployer) {
